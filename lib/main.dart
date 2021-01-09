75f71970950832a711af26661b6319dd70c8fac4
KittiST <dangst@trMacbook.true.true>
2021-01-09 18:56:59 +0700

    add rotate and icon

--- lib/main.dart
+++ lib/main.dart
@@ -40,17 +40,14 @@ class _MyHomePageState extends State<MyHomePage> {
   double _scaleFactor = 2.0;
   double _baseScaleFactor = 1.0;
   bool isEdit = false;
+  int _selectedRotate = 0;
+  String initText = 'Tap to edit';
 
   @override
   void initState() {
     super.initState();
-    _textController.text = 'Tap to edit';
-    // _textController.addListener(_printLatestValue);
+    _textController.text = initText;
   }
-
-  // _printLatestValue() {
-  //   print("Second text field: ${_textController.text}");
-  // }
 
   void _changeColor() {
     setState(() {
@@ -61,6 +58,15 @@ class _MyHomePageState extends State<MyHomePage> {
         _selectedColor = 1;
       else
         _selectedColor = 0;
+    });
+  }
+
+  void _changeRotate() {
+    setState(() {
+      if (_selectedRotate == 0)
+        _selectedRotate = 1;
+      else
+        _selectedRotate = 0;
     });
   }
 
@@ -81,78 +87,96 @@ class _MyHomePageState extends State<MyHomePage> {
           _scaleFactor = _baseScaleFactor * scaleDetails.scale;
         });
       },
-      onTap: () => setState(() => isEdit = !isEdit),
+      onTap: () {
+        if (_textController.text == initText) _textController.text = '';
+        setState(() => isEdit = !isEdit);
+      },
       child: Scaffold(
         backgroundColor: _colorList[_selectedBGColor],
-        body: Stack(
-          children: [
-            if (isEdit)
-              Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    color: Colors.black.withOpacity(0.5),
-                    child: Center(
-                      child: TextField(
-                        autofocus: true,
-                        textAlign: TextAlign.center,
-                        controller: _textController,
-                        style: TextStyle(color: Colors.white, fontSize: 24.0),
-                        decoration: InputDecoration(
-                            fillColor: Colors.white, border: InputBorder.none),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        FlatButton(
-                          onPressed: () => setState(() => isEdit = !isEdit),
-                          child: Text('Done',
-                              style: TextStyle(
-                                  color: Colors.white, fontSize: 20.0)),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            if (!isEdit)
-              SafeArea(
-                child: Stack(
-                  alignment: Alignment.topRight,
+        body: SafeArea(
+          child: Stack(
+            children: [
+              if (isEdit)
+                Stack(
+                  alignment: Alignment.bottomCenter,
                   children: [
-                    FlatButton(
-                        minWidth: 10,
-                        onPressed: _changeColor,
-                        child:
-                            //   Text('Color',
-                            //       style: TextStyle(
-                            //           color: _colorList[_selectedColor],
-                            //           fontSize: 16.0)),
-                            // )
-                            Icon(
-                          Icons.color_lens_outlined,
-                          color: _colorList[_selectedColor],
-                          size: 30.0,
-                        )),
                     Container(
                       padding: EdgeInsets.all(16.0),
+                      color: Colors.black.withOpacity(0.5),
                       child: Center(
-                        child: Text(
-                          '${_textController.text}',
-                          textScaleFactor: _scaleFactor,
-                          style: TextStyle(color: _colorList[_selectedColor]),
+                        child: TextField(
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
+                          autofocus: true,
+                          textAlign: TextAlign.center,
+                          controller: _textController,
+                          style: TextStyle(color: Colors.white, fontSize: 24.0),
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              border: InputBorder.none),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          FlatButton(
+                            onPressed: () => setState(() => isEdit = !isEdit),
+                            child: Text('Done',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20.0)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              if (!isEdit)
+                Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        FlatButton(
+                            minWidth: 10,
+                            onPressed: _changeRotate,
+                            child: Icon(
+                              Icons.screen_rotation_outlined,
+                              color:
+                                  _colorList[_selectedColor].withOpacity(0.5),
+                              size: 30.0,
+                            )),
+                        FlatButton(
+                            minWidth: 10,
+                            onPressed: _changeColor,
+                            child: Icon(
+                              Icons.color_lens_outlined,
+                              color:
+                                  _colorList[_selectedColor].withOpacity(0.5),
+                              size: 30.0,
+                            )),
+                      ],
+                    ),
+                    RotatedBox(
+                      quarterTurns: _selectedRotate,
+                      child: Container(
+                        padding: EdgeInsets.all(16.0),
+                        child: Center(
+                          child: Text(
+                            '${_textController.text}',
+                            textScaleFactor: _scaleFactor,
+                            style: TextStyle(color: _colorList[_selectedColor]),
+                          ),
                         ),
                       ),
                     ),
                   ],
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
